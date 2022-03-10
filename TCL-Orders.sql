99827b47001590feef30c3c47e91b1556d98f8ba
IoanMarghioala <100705128+IoanMarghioala@users.noreply.github.com>
2022-03-10 19:25:55 +0200

    Update TCL-Orders.sql
    Update with multiple queries

--- TCL-Orders.sql
+++ TCL-Orders.sql
@@ -5,3 +5,12 @@ START TRANSACTION;
 	INSERT INTO orderdetails (`orderNumber`,`productCode`,`quantityOrdered`,`priceEach`,`orderLineNumber`) VALUES (10162,'S10_4757',21,'80.14',2);
 	DELETE FROM payments WHERE customerNumber = 415;
 COMMIT;
+
+
+//---------------- Transaction that updates the missing comments with a message and displays the new table -------------
+START TRANSACTION;
+  UPDATE orders SET comments = 'There are no comments' WHERE comments is NULL;
+  UPDATE customers SET customers.addressLine2 = 'There is no other address' 	where addressLine2 is NULL;
+  SELECT orderNumber, comments FROM orders;
+  SELECT customerNumber, addressLine2 FROM customers;
+COMMIT;
